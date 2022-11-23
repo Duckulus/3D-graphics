15239aab657985cfe356463211b978c2d4e9fe4c
Amin <amin.haddou@frg.wwschool.de>
2022-11-23 07:48:28 +0100

    move triangle transformation logic out of Processing

--- src/main/java/org/example/Processing.java
+++ src/main/java/org/example/Processing.java
@@ -10,7 +10,7 @@ public class Processing extends PApplet {
 
     private List<Triangle> meshCube;
     private Mat4x4 projMat;
-    float time;
+    float theta;
 
     @Override
     public void settings() {
@@ -67,75 +67,21 @@ public class Processing extends PApplet {
 
     @Override
     public void draw() {
-        time += 0.02;
+        theta += 0.02;
         background(Color.BLACK.getRGB());
 
-        Mat4x4 rotMatX = new Mat4x4();
-        Mat4x4 rotMatZ = new Mat4x4();
-
-        float xRotationDiff = 0.2f;
-
-        rotMatX.m[0][0] = 1;
-        rotMatX.m[1][1] = (float) Math.cos(time * xRotationDiff);
-        rotMatX.m[1][2] = (float) Math.sin(time * xRotationDiff);
-        rotMatX.m[2][1] = (float) -Math.sin(time * xRotationDiff);
-        rotMatX.m[2][2] = (float) Math.cos(time * xRotationDiff);
-        rotMatX.m[3][3] = 1;
-
-        rotMatZ.m[0][0] = (float) Math.cos(time);
-        rotMatZ.m[0][1] = (float) Math.sin(time);
-        rotMatZ.m[1][0] = (float) -Math.sin(time);
-        rotMatZ.m[1][1] = (float) Math.cos(time);
-        rotMatZ.m[2][2] = 1;
-        rotMatZ.m[3][3] = 1;
-
+        float xRotationDiff = 0.5f;
 
         for (Triangle tri : meshCube) {
-
-            Triangle rotatedZ = new Triangle(
-                    tri.p[0].multiplyMatrix4(rotMatZ),
-                    tri.p[1].multiplyMatrix4(rotMatZ),
-                    tri.p[2].multiplyMatrix4(rotMatZ)
-            );
-
-            Triangle rotatedZX = new Triangle(
-                    rotatedZ.p[0].multiplyMatrix4(rotMatX),
-                    rotatedZ.p[1].multiplyMatrix4(rotMatX),
-                    rotatedZ.p[2].multiplyMatrix4(rotMatX)
-            );
-
-            Triangle translated = new Triangle(
-                    new Vec3d(rotatedZX.p[0].x, rotatedZX.p[0].y, rotatedZX.p[0].z),
-                    new Vec3d(rotatedZX.p[1].x, rotatedZX.p[1].y, rotatedZX.p[1].z),
-                    new Vec3d(rotatedZX.p[2].x, rotatedZX.p[2].y, rotatedZX.p[2].z)
-            );
-
-            translated.p[0].z += 3;
-            translated.p[1].z += 3;
-            translated.p[2].z += 3;
-
-            Triangle projected = new Triangle(
-                    translated.p[0].multiplyMatrix4(projMat),
-                    translated.p[1].multiplyMatrix4(projMat),
-                    translated.p[2].multiplyMatrix4(projMat)
-            );
-
-            projected.p[0].x += 1.0;
-            projected.p[0].y += 1.0;
-            projected.p[1].x += 1.0;
-            projected.p[1].y += 1.0;
-            projected.p[2].x += 1.0;
-            projected.p[2].y += 1.0;
-
-            projected.p[0].x *= 0.5f * width;
-            projected.p[0].y *= 0.5f * height;
-            projected.p[1].x *= 0.5f * width;
-            projected.p[1].y *= 0.5f * height;
-            projected.p[2].x *= 0.5f * width;
-            projected.p[2].y *= 0.5f * height;
-
             stroke(Color.WHITE.getRGB());
-            drawTriangle(projected);
+            drawTriangle(
+                    tri
+                            .rotateZ(theta)
+                            .rotateX(theta*xRotationDiff)
+                            .translateZ(3f)
+                            .project(projMat)
+                            .center(width, height)
+            );
         }
     }
 
--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -9,4 +9,80 @@ public class Triangle {
                 a, b, c
         };
     }
+
+    public Triangle rotateZ(float theta) {
+        Mat4x4 rotMatZ = new Mat4x4();
+        rotMatZ.m[0][0] = (float) Math.cos(theta);
+        rotMatZ.m[0][1] = (float) Math.sin(theta);
+        rotMatZ.m[1][0] = (float) -Math.sin(theta);
+        rotMatZ.m[1][1] = (float) Math.cos(theta);
+        rotMatZ.m[2][2] = 1;
+        rotMatZ.m[3][3] = 1;
+        return new Triangle(
+                this.p[0].multiplyMatrix4(rotMatZ),
+                this.p[1].multiplyMatrix4(rotMatZ),
+                this.p[2].multiplyMatrix4(rotMatZ)
+        );
+    }
+
+    public Triangle rotateX(float theta) {
+        Mat4x4 rotMatX = new Mat4x4();
+        rotMatX.m[0][0] = 1;
+        rotMatX.m[1][1] = (float) Math.cos(theta);
+        rotMatX.m[1][2] = (float) Math.sin(theta);
+        rotMatX.m[2][1] = (float) -Math.sin(theta);
+        rotMatX.m[2][2] = (float) Math.cos(theta);
+        rotMatX.m[3][3] = 1;
+        return new Triangle(
+                this.p[0].multiplyMatrix4(rotMatX),
+                this.p[1].multiplyMatrix4(rotMatX),
+                this.p[2].multiplyMatrix4(rotMatX)
+        );
+    }
+
+    public Triangle translateZ(float amount) {
+        Triangle translated = this.copy();
+        translated.p[0].z += amount;
+        translated.p[1].z += amount;
+        translated.p[2].z += amount;
+        return translated;
+    }
+
+    public Triangle project(Mat4x4 projMat) {
+        return new Triangle(
+                this.p[0].multiplyMatrix4(projMat),
+                this.p[1].multiplyMatrix4(projMat),
+                this.p[2].multiplyMatrix4(projMat)
+        );
+
+    }
+
+    public Triangle center(float width, float height) {
+        Triangle centered = this.copy();
+
+        centered.p[0].x += 1.0;
+        centered.p[0].y += 1.0;
+        centered.p[1].x += 1.0;
+        centered.p[1].y += 1.0;
+        centered.p[2].x += 1.0;
+        centered.p[2].y += 1.0;
+
+        centered.p[0].x *= 0.5f * width;
+        centered.p[0].y *= 0.5f * height;
+        centered.p[1].x *= 0.5f * width;
+        centered.p[1].y *= 0.5f * height;
+        centered.p[2].x *= 0.5f * width;
+        centered.p[2].y *= 0.5f * height;
+
+        return centered;
+    }
+
+    public Triangle copy() {
+        return new Triangle(
+                new Vec3d(this.p[0].x, this.p[0].y, this.p[0].z),
+                new Vec3d(this.p[1].x, this.p[1].y, this.p[1].z),
+                new Vec3d(this.p[2].x, this.p[2].y, this.p[2].z)
+        );
+    }
+
 }
